757b9e6e5ec2ca05b2bb90bab64d388aa4793f56
mfdev <mfdevbox@gmail.com>
2023-05-13 00:14:13 +0300

    subnav fixes

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -8,6 +8,7 @@ export default function Header() {
     const location = useLocation()
     const deviceType = useSelector(state => state.settings.deviceType)
     const logotypeRef = useRef()
+    const subNavigationRef = useRef()
 
     const handleLogotypeClick = () => {
         logotypeRef.current.classList.add(styles.clicked)
@@ -16,40 +17,99 @@ export default function Header() {
         }, 300)
     }
 
+    const handleSubNavMouseEnter = () => {
+        subNavigationRef.current.classList.remove(styles.closed)
+    }
+    const handleSubNavMouseLeave = () => {
+        subNavigationRef.current.classList.add(styles.closed)
+    }
+    const handleSubNavLinkClick = () => {
+        subNavigationRef.current.classList.add(styles.closed)
+    }
+
     return (
         <header className={styles[deviceType]}>
             {deviceType === 'desktop' && (
                 <div className={styles.wrapper}>
-                    <Link to='/' className={styles.logotype} ref={logotypeRef} onClick={handleLogotypeClick} />
+                    <Link
+                        to='/'
+                        className={styles.logotype}
+                        ref={logotypeRef}
+                        onClick={handleLogotypeClick}
+                    />
 
                     <div className={styles.navigation}>
-                        <Link to='/pizzas' className={location.pathname === '/pizzas' ? styles.active : undefined}>
+                        <Link
+                            to='/pizzas'
+                            className={location.pathname === '/pizzas' ? styles.active : undefined}
+                        >
                             Пиццы
                         </Link>
-                        <Link to='/snacks' className={location.pathname === '/snacks' ? styles.active : undefined}>
+                        <Link
+                            to='/snacks'
+                            className={location.pathname === '/snacks' ? styles.active : undefined}
+                        >
                             Закуски
                         </Link>
-                        <Link to='/desserts' className={location.pathname === '/desserts' ? styles.active : undefined}>
+                        <Link
+                            to='/desserts'
+                            className={location.pathname === '/desserts' ? styles.active : undefined}
+                        >
                             Десерты
                         </Link>
-                        <Link to='/drinks' className={location.pathname === '/drinks' ? styles.active : undefined}>
+                        <Link
+                            to='/drinks'
+                            className={location.pathname === '/drinks' ? styles.active : undefined}
+                        >
                             Напитки
                         </Link>
                     </div>
 
-                    <div className={styles.subnavigation}>
+                    <div
+                        className={styles.subnavigation}
+                        onMouseEnter={handleSubNavMouseEnter}
+                        onMouseLeave={handleSubNavMouseLeave}
+                    >
                         <button>Другое</button>
-                        <div>
-                            <Link to='/delivery'>Доставка</Link>
-                            <Link to='/discounts'>Акции</Link>
-                            <Link to='/questions'>F.A.Q.</Link>
+                        <div
+                            ref={subNavigationRef}
+                            className={styles.closed}
+                        >
+                            <Link
+                                to='/delivery'
+                                className={location.pathname === '/delivery' ? styles.active : undefined}
+                                onClick={handleSubNavLinkClick}
+                            >
+                                Доставка
+                            </Link>
+                            <Link
+                                to='/discounts'
+                                className={location.pathname === '/discounts' ? styles.active : undefined}
+                                onClick={handleSubNavLinkClick}
+                            >
+                                Акции
+                            </Link>
+                            <Link
+                                to='/questions'
+                                className={location.pathname === '/questions' ? styles.active : undefined}
+                                onClick={handleSubNavLinkClick}
+                            >
+                                F.A.Q.
+                            </Link>
                         </div>
                     </div>
 
                     <BasketButton deviceType='desktop' />
                 </div>
             )}
-            {deviceType === 'mobile' && <Link to='/' className={styles.logotype} ref={logotypeRef} onClick={handleLogotypeClick} />}
+            {deviceType === 'mobile' && (
+                <Link
+                    to='/'
+                    className={styles.logotype}
+                    ref={logotypeRef}
+                    onClick={handleLogotypeClick}
+                />
+            )}
         </header>
     )
 }
